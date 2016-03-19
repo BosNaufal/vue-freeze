abb97c090ab3ef658db08475e9cc1b5c39cc11f9
Naufal <bosnaufalemail@gmail.com>
2016-03-19 07:18:37 +0700

    Change The API

--- src/vue-freeze.js
+++ src/vue-freeze.js
@@ -7,87 +7,90 @@
 */
 import Freezer from './freezer.min.js'
 
+
 (function(){
 
-	var VueFreeze = {
+  var VueFreeze = {
 
-		install(Vue,opt){
+    install(Vue,opt){
 
 
-			// Check the Freezer
-			if( typeof Freezer == 'undefined' ) console.warn('[Vue Freeze]: You Must Install Freezer.js firs!')
+      // Check the Freezer
+      if( typeof Freezer == 'undefined' ) console.warn('[Vue Freeze]: You Must Install Freezer.js firs!')
 
-			var plugin = this
+      var plugin = this
 
-			// Plugin Store
+      // Plugin Store
       if(typeof opt  == 'undefined') {
         console.warn('[Vue Freeze]: Please Specify the store! Via Vue.use Options!')
         return false
       }
-			else {
+      else {
         // live update?
         var live = opt.live ? opt.live : true
+
         // make our store!
         var store =  new Freezer(opt.state,{live:live})
-				// binding the store
+
+        // binding the action
         opt.action(store)
       }
 
-			// Make a mixin
-			plugin.mixin = {}
+      // Make a data become mutable
+      var mutable = (data) => JSON.parse(JSON.stringify(data))
 
-			// Mixin Methods For vm instance
-			plugin.mixin.methods = {
-				// For mutable data
-				mutable(data){
-					return JSON.parse(JSON.stringify(data))
-				},
+      // Make a mixin
+      plugin.mixin = {}
 
-				// set the vm $state to new state
-		    updateState(old,val){
-					this.$set('$state',store.get())
-		    }
-			}
+      // Mixin Methods For vm instance
+      plugin.mixin.methods = {
+        // set the vm state to new state
+        updateState(old,val){
+          this.$set('state',mutable(store.get()))
+        }
+      }
 
-			// Mixin Created Event
-			plugin.mixin.created = function() {
+      // Mixin Created Event
+      plugin.mixin.created = function() {
         var me = this
 
-				// Make Methods
-				Vue.util.defineReactive(this,'$store',store)
-				Vue.util.defineReactive(this,'$state',store.get())
-		    Vue.util.defineReactive(this,'$action', function(eventName,args){
-					// trigger freeze event with pass the old value at the end
-					store.trigger(eventName,args,me.$state)
-				})
+        // set global state as internal state
+        Vue.util.defineReactive(this,'state',mutable(store.get()))
 
-				// When Store Updated~
-		    this.$store.on('update', function (val) {
-					// Update the state
-		      me.updateState(me.$state,val)
-		    })
-			}
+        // Make Methods
+        Vue.util.defineReactive(this,'$store',store)
+        Vue.util.defineReactive(this,'$action', function(eventName,arg){
+          // trigger freezer event with pass the old value at the end
+          store.trigger(eventName,arg,me.state)
+        })
 
-			// Merge mixin to VM via vue options
-			Vue.options = Vue.util.mergeOptions(Vue.options, plugin.mixin)
+        // When Store Updated~
+        me.$store.on('update', function (val) {
+          // Update the state
+          me.updateState(me.state,val)
+        })
+      }
 
-		} // install()
+      // Merge mixin to VM via vue options
+      Vue.options = Vue.util.mergeOptions(Vue.options, plugin.mixin)
+
+    } // install()
 
 
-	} // VueFreeze
+  } // VueFreeze
 
 
-	// If support node / ES6 module
-	if( typeof module === 'object' && module.exports ){
-		module.exports = VueFreeze
-	}
-	// if using require js
-	if (typeof define === 'function' && define.amd) {
-		define(() => { return VueFreeze })
-	}
-	// if script loaded by script tag in HTML file
-	if (typeof window !== undefined) {
-		window.VueFreeze = VueFreeze
-	}
+  // If support node / ES6 module
+  if( typeof module === 'object' && module.exports ){
+    module.exports = VueFreeze
+  }
+  // if using require js
+  if (typeof define === 'function' && define.amd) {
+    define(() => { return VueFreeze })
+  }
+  // if script loaded by script tag in HTML file
+  if (typeof window !== undefined) {
+    return window.VueFreeze = VueFreeze
+  }
 
 })();
